91ed35b720ad4c4c87743aa0084922a3fc28d7f4
Bougou <bougou@126.com>
2021-09-01 15:52:14 +0800

    add bundle subcommand

---
+++ pkg/commands/bundle/bundle.go
@@ -0,0 +1,84 @@
+package bundle
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+
+	"github.com/bougou/gopkg/common"
+	"github.com/bougou/sail/pkg/models"
+	"github.com/spf13/cobra"
+)
+
+func NewCmdBundle(sailOption *models.SailOption) *cobra.Command {
+	o := NewBundleOptions(sailOption)
+
+	cmd := &cobra.Command{
+		Use:   "bundle",
+		Short: "bundle packages of a product",
+		Long:  "bundle packages of a product",
+		Run: func(cmd *cobra.Command, args []string) {
+			common.CheckErr(o.Complete(cmd, args))
+			common.CheckErr(o.Validate())
+			common.CheckErr(o.Run())
+		},
+	}
+
+	defaultProductName := ""
+	cmd.Flags().StringVarP(&o.productName, "product", "p", defaultProductName, "the product name")
+	cmd.MarkFlagRequired("playbook")
+
+	return cmd
+}
+
+type BundleOptions struct {
+	productName string
+	productDir  string
+
+	args        []string
+	productsDir string
+	targetsDir  string
+	sailOption  *models.SailOption
+}
+
+func NewBundleOptions(sailOption *models.SailOption) *BundleOptions {
+	return &BundleOptions{
+		sailOption: sailOption,
+	}
+}
+
+func (o *BundleOptions) Complete(cmd *cobra.Command, args []string) error {
+	if o.productName == "" {
+		return errors.New("product name must not be empty")
+	}
+
+	o.productDir = path.Join(o.sailOption.ProductsDir, o.productName)
+	stat, err := os.Stat(o.productDir)
+	if err != nil || !stat.IsDir() {
+		msg := fmt.Sprintf("Not found dir of product, %s does not exist", o.productDir)
+		return errors.New(msg)
+	}
+
+	return nil
+}
+
+func (o *BundleOptions) Validate() error {
+	return nil
+}
+
+func (o *BundleOptions) Run() error {
+	product := models.NewProduct(o.productName, o.sailOption.ProductsDir)
+	if err := product.Init(); err != nil {
+		msg := fmt.Sprintf("product init failed, err: %s", err)
+		return errors.New(msg)
+	}
+
+	components := product.ComponentList()
+	fmt.Printf("The product %s contains the following components:\n", o.productName)
+	for _, c := range components {
+		fmt.Printf("- %s\n", c)
+	}
+
+	return nil
+}
